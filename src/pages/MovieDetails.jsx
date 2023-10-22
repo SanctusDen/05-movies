30575b3f9e430b67dbef2c61d4a86ccd108b8289
Dionisii <121027564+SanctusDen@users.noreply.github.com>
2023-10-22 21:29:01 +0200

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchAllDetails } from 'services/api';
 
 const MovieDetails = () => {
-  return <div>MovieDetails</div>;
+  const [loading, setLoading] = useState(false);
+  const [movies, setMovies] = useState({});
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    const details = async () => {
+      setLoading(true);
+      try {
+        const { results } = await fetchAllDetails();
+        setMovies(results);
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    details();
+  }, []);
+  return (
+    <div>
+      <h1>{movieId}</h1>
+    </div>
+  );
 };
 
 export default MovieDetails;
