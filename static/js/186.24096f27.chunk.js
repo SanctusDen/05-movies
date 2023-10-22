e058a01b4fb5e2413a0e4261b2bc74472a85ea60
SanctusDen <121027564+SanctusDen@users.noreply.github.com>
2023-10-22 13:52:35 +0000

    Deploying to gh-pages from @ SanctusDen/goit-react-hw-05-movies@8f63b1df9ca85659db3d740fba9f2087d4b8445a 🚀

---
+++ static/js/186.24096f27.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[186],{186:function(e,t,a){a.r(t);t.default=function(){}}}]);
+//# sourceMappingURL=186.24096f27.chunk.js.map
---
+++ static/js/884.92d42b47.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[884],{884:function(e,t,r){r.r(t);r(791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"MovieDetails"})}}}]);
+//# sourceMappingURL=884.92d42b47.chunk.js.map
